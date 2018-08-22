e9d9f5d5b08c7ae480989b1995dbb78b91a97665
Yomairy Pena <misspena21@gmail.com>
2018-08-22 19:20:59 -0400

    add seed file for users and accounts using faker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_230042) do
+ActiveRecord::Schema.define(version: 2018_08_22_222050) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "phone_number"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
