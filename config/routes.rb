f8d0d5ef8be3428dc324c3f8c1226234d558a104
Yomairy Pena <misspena21@gmail.com>
2018-08-22 17:08:30 -0400

    add check boxes to have next to contact accounts info

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users
-  resources :tasks, only: [:index, :create, :destroy, :update]
+  resources :accounts, only: [:index, :create, :destroy, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
