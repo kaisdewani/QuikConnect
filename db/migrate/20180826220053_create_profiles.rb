b96c86212d1b45998151d376417409c0a0dcf503
Kais Dewani <kaisdewani@yahoo.com>
2018-08-26 19:51:00 -0400

    remove email address and redo migration

--- db/migrate/20180826220053_create_profiles.rb
+++ db/migrate/20180826220053_create_profiles.rb
@@ -3,7 +3,6 @@ class CreateProfiles < ActiveRecord::Migration[5.2]
     create_table :profiles do |t|
       t.string :first_name
       t.string :last_name
-      t.string :personal_email
       t.string :personal_phone
       t.string :personal_address
 
