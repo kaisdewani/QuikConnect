447ce3be2f06a2d987a0543d050fec21efc75dc7
Kais Dewani <kaisdewani@yahoo.com>
2018-08-26 18:01:27 -0400

    create profile model

---
+++ db/migrate/20180826220053_create_profiles.rb
@@ -0,0 +1,13 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :personal_email
+      t.string :personal_phone
+      t.string :personal_address
+
+      t.timestamps
+    end
+  end
+end
