60b5e06c5b87f0888217054358cdc957d6295c7d
Yomairy Pena <misspena21@gmail.com>
2018-08-30 16:00:20 -0400

    making text form, working text buttton, and able to receive text

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'profiles#edit'
+  post '/messages/text', to: 'messages#text'
   devise_for :users
   resource :profiles
   resources :accounts
+  resources :messages, only: [:create, :index, :new]
+
 end
