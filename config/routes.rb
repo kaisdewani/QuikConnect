579820e2d7e0a2f63fe9c2052f29dade6521b726
Kais Dewani <kaisdewani@yahoo.com>
2018-08-31 14:09:08 -0400

    simplified routes using except restriction

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'profiles#edit'
   devise_for :users
   resource :profiles
-  resources :accounts
+  resources :accounts, except: [:show]
+  resource :shares, only: [:create, :show]
 end
